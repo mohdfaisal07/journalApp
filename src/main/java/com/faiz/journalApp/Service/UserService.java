bf66204174e83e80ac706bff52d98ffa18687be0
mohdfaisal07 <faisalsyedskr@gmail.com>
2026-01-19 00:18:16 +0530

    Added spring security update

--- src/main/java/com/faiz/journalApp/Service/UserService.java
+++ src/main/java/com/faiz/journalApp/Service/UserService.java
@@ -4,8 +4,13 @@ import com.faiz.journalApp.Entity.User;
 import com.faiz.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.Encrypted;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,7 +19,11 @@ public class UserService {
 @Autowired
     private UserRepository userRepository;
 
+private static final PasswordEncoder passwordEncoder= new BCryptPasswordEncoder();
+
 public void saveUser(User user){
+   user.setPassword( passwordEncoder.encode(user.getPassword()));
+   user.setRoles(Arrays.asList("USER"));
     userRepository.save(user);
 }
 
