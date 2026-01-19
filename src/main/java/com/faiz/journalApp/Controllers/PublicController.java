10ec311269a794415bc6eaccd8a1092b74d4eb0b
mohdfaisal07 <faisalsyedskr@gmail.com>
2026-01-19 23:55:12 +0530

    Added spring security update

--- src/main/java/com/faiz/journalApp/Controllers/PublicController.java
+++ src/main/java/com/faiz/journalApp/Controllers/PublicController.java
@@ -1,10 +1,12 @@
 package com.faiz.journalApp.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheck {
+@RequestMapping("/public")
+public class PublicController {
     @GetMapping("/health-check")
     public String healthCheck(){
         return "OK";
